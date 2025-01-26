7d8d93caf1fbeb36d7783895f23c52f718de3ad3
Tom Childs <childsy123@gmail.com>
2025-01-26 12:46:20 +0000

    Removed subscription advert

--- free-european-plugin/content.js
+++ free-european-plugin/content.js
@@ -10,12 +10,21 @@ const unlock = () => {
     // Get the shown element
     const shownContent = document.querySelector('.pp-subs__post-start');
 
-    // Inject the content of the hidden element into the content of the shown element
+    // Inject the content of the hidden element into the content of the shown element.
+    // Add an empty paragraph to the end to prevent the gradient at the bottom of the element from obscuring the final paragraph of text. 
     if (hiddenContent && shownContent) {
         shownContent.innerHTML += hiddenContent.innerHTML;
+        shownContent.appendChild(document.createElement('p'));
     } else {
         console.log('Unlock failed.')
     }
+    
+    // Rmove the element containing the subscription advert 
+    const subscriptionAdvert = document.querySelector('#view-offer');
+    if (subscriptionAdvert) {
+      subscriptionAdvert.remove();
+    }
+
 }
 
 // Wait 1000ms to ensure the page content is fully loaded before unlocking the page
